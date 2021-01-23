d4c038935ec19ae38bd48620adbf165d62013331
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-23 19:26:21 +0100

    Add initial project files

--- cs-lab-serializacja-szyfrowanie-GraZgadywanka/Program.cs
+++ cs-lab-serializacja-szyfrowanie-GraZgadywanka/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using GraZaDuzoZaMalo.Model;
 
-namespace cs_lab_serializacja_szyfrowanie_GraZgadywanka
+namespace AppGraZaDuzoZaMaloCLI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            (new KontrolerCLI()).Uruchom();
         }
+
     }
 }
