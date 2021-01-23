aad3071f1d66af7b534a9c0637efcacfee97ed5b
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-23 19:22:32 +0100

    Add initial files

---
+++ cs-lab-serializacja-szyfrowanie-GraZgadywanka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_lab_serializacja_szyfrowanie_GraZgadywanka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
