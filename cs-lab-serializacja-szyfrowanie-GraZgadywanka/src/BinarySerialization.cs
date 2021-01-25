c501a0171df02fbc4caf9ad7a73d4c58225e849a
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-25 21:12:42 +0100

    Create BinarySerialization.cs

---
+++ cs-lab-serializacja-szyfrowanie-GraZgadywanka/src/BinarySerialization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace cs_lab_serializacja_szyfrowanie_GraZgadywanka.src
+{
+    public static class BinarySerialization
+    {
+        public static byte[] SerializeToMemory<T> (T obj)
+        {
+            using var memStream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+
+            formatter.Serialize(memStream, obj);
+            return memStream.ToArray();
+        }
+
+        public static T DeserializeFromMemory<T>(byte[] streamObj)
+        {
+            using var memStream = new MemoryStream(streamObj);
+            var formatter = new BinaryFormatter();
+            return (T)formatter.Deserialize(memStream);
+        }
+    }
+}
